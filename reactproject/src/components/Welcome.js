278a18c2fe0e8f65371d55a787f73827dfc4ebdc
Rosu Mihai <roshu.mihai@yahoo.com>
2023-07-06 03:16:06 +0300

    updated social media profiles

--- reactproject/src/components/Welcome.js
+++ reactproject/src/components/Welcome.js
@@ -37,9 +37,14 @@ class Welcome extends Component {
             <div id="quote-box">
                 <p id="text">{this.state.message}</p>
                 <p id="author-quote">{this.state.author}</p>
-                <a id="tweet-quote" href="#" target="_blank">
-                    <img id="tweet-img" src="https://www.iconninja.com/files/383/986/347/tweeting-tweet-twitter-bird-logo-icon.png" />
-                </a>
+                
+                <h1 className='profiles'>My Social Media Profiles</h1>
+                <ul className='ul-profiles'>
+                    <li><a href="https://www.facebook.com/profile.php?id=100009359224502" target="_blank">Facebook</a></li>
+                    <li><a href="https://www.instagram.com/rosu.mihai/" target="_blank">Instagram</a></li>
+                    <li><a href="https://github.com/roshumihai" target="_blank">GitHub</a></li>
+                    <li><a href="linkedin.com/in/mihai-rosu-4bba5456" target="_blank">Linkedin</a></li>
+                </ul>
                 <button id="new-quote" onClick={this.handleChange}>New quote</button>
             </div>
         )
